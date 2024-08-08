bb2d6aa478c4967d6ea8609b8b032c5fe0507608
Natalia <nat_cvc@hotmail.com>
2024-08-07 22:07:20 -0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,61 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo')
+    photo.src = profileData.photo
+    photo.alt = profileData.name
+
+    const name = document.getElementById('profile.name')
+    name.innerText = profileData.name
+
+    const job = document.getElementById('profile.job')
+    job.innerText = profileData.job
+
+    const location = document.getElementById('profile.location')
+    location.innerText = profileData.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.innerText = profileData.phone
+    phone.href = `tel:${profileData.phone}`
+
+    const email = document.getElementById('profile.email')
+    email.innerText = profileData.email
+    email.href = `mailto:${profileData.email}`
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateLanguages(profileData) {
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
+function updateEducação(profileData) {
+    const Educação = document.getElementById('profile.Educação')
+    Educação.innerHTML = profileData.Educação.map(Educação => {
+       
+    }).join('')
+}
+
+function updatePortfolio(profileData) {
+    const Portfolio = document.getElementById('profile.Portfolio')
+    Portfolio.innerHTML = profileData.Portfolio.map(Portfolio => {
+        return `
+            <li>
+                <h3 class="title">${Portfolioname}</h3>
+                <p>${Portfolio.description}</p>
+            </li>
+        `
+    }).join('')
+}
+
+(async () => {
+    const profileData = await fetchProfileData()
+    updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
+    updateLanguages(profileData)
+    updateEducação(profileData)
+    updatePortfolio(profileData)
+})()
